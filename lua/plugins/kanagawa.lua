c0ca7840b950fac82ff9666dcf4c0cbcaab31117
Sunight01 <kevin.bmorillas@gmail.com>
2025-01-05 17:21:55 -0300

    feat: first commit
    my config for nvim! :)

---
+++ lua/plugins/kanagawa.lua
@@ -0,0 +1,30 @@
+return {
+  "rebelot/kanagawa.nvim",
+  config = function()
+    require("kanagawa").setup({
+      compile = true, -- Habilita la compilación del esquema de colores
+      undercurl = true, -- Habilita undercurls
+      commentStyle = { italic = true },
+      functionStyle = {},
+      keywordStyle = { italic = true },
+      statementStyle = { bold = true },
+      typeStyle = {},
+      transparent = true, -- No establecer color de fondo
+      dimInactive = true, -- Atenuar ventana inactiva :h hl-NormalNC
+      terminalColors = true, -- Definir vim.g.terminalcolor{0,17}
+      colors = { -- Agregar/modificar colores de tema y paleta
+        palette = {},
+        theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+      },
+      overrides = function(colors) -- Agregar/modificar destacados
+        return {}
+      end,
+      theme = "wave", -- Cargar tema "wave" cuando la opción 'background' no está configurada
+      background = { -- Mapear el valor de la opción 'background' a un tema
+        dark = "wave", -- Intenta "dragon"!
+        light = "wave",
+      },
+    })
+    vim.cmd("colorscheme kanagawa")
+  end,
+}
